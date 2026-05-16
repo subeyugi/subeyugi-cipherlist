00eeaf9bbc28b0530c1d932a815a55e4765ba76e
subeyugi <164650867+subeyugi@users.noreply.github.com>
2026-05-16 14:02:49 +0900

    Delete functions/api directory

--- functions/api/user.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { DB } = context.env;
-
-  const result = await DB.prepare(
-    "SELECT * FROM users"
-  ).all();
-
-  return Response.json(result.results);
-}
