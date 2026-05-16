7925ed28bd353f322608ef2f70f7c9079c00d690
subeyugi <164650867+subeyugi@users.noreply.github.com>
2026-05-16 11:04:12 +0900

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request: Request, env: Env) {
+    const { pathname } = new URL(request.url);
+
+    if (pathname === "/api/beverages") {
+      const { results } = await env.DB.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?"
+      )
+        .bind("Bs Beverages")
+        .all();
+      return Response.json(results);
+    }
+
+    return new Response(
+      "Call /api/beverages to see everyone who works at Bs Beverages"
+    );
+  },
+};
